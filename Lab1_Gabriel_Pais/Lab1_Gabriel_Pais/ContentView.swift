1f3bd8087a732bd3a96bcf81a801213ff985ce1f
TheMith <77514217+TheMithh@users.noreply.github.com>
2025-02-13 18:18:20 -0500

    Add prime number checker

--- Lab1_Gabriel_Pais/Lab1_Gabriel_Pais/ContentView.swift
+++ Lab1_Gabriel_Pais/Lab1_Gabriel_Pais/ContentView.swift
@@ -106,6 +106,15 @@ struct ContentView: View {
         currentNumber = 0
     }
 
+    func isPrime(_ number: Int) -> Bool {
+        guard number > 1 else { return false }
+        for i in 2..<Int(sqrt(Double(number))) + 1 {
+            if number % i == 0 { return false }
+        }
+        return true
+    }
+
+
 }
 
 struct ContentView_Previews: PreviewProvider {
