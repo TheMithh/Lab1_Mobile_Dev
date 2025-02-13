16f283fdf90e8ab0410098173a7dbfd757086ba9
TheMith <77514217+TheMithh@users.noreply.github.com>
2025-02-13 17:32:10 -0500

    Project Initialization

---
+++ Lab1_Gabriel_Pais/Lab1_Gabriel_Pais/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Lab1_Gabriel_Pais
+//
+//  Created by viorel pais on 2025-02-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lab1_Gabriel_Pais/Lab1_Gabriel_Pais/Lab1_Gabriel_PaisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Gabriel_PaisApp.swift
+//  Lab1_Gabriel_Pais
+//
+//  Created by viorel pais on 2025-02-13.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Gabriel_PaisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
