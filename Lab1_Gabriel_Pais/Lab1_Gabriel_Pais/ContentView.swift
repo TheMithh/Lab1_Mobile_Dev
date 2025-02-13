d1fce9ee2524d9de6214d0ba94ed012d46643b6f
TheMith <77514217+TheMithh@users.noreply.github.com>
2025-02-13 17:34:12 -0500

    Add game state variables

--- Lab1_Gabriel_Pais/Lab1_Gabriel_Pais/ContentView.swift
+++ Lab1_Gabriel_Pais/Lab1_Gabriel_Pais/ContentView.swift
@@ -8,15 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+    @State private var currentNumber = 0
+    @State private var gameStarted = false
+    @State private var showResult = false
+    @State private var isCorrect = false
+    @State private var correctCount = 0
+    @State private var wrongCount = 0
+    @State private var attempts = 0
+    @State private var timer: Timer?
+    @State private var showScoreAlert = false
+    @State private var lastCorrectCount: Int? = nil
+    @State private var lastWrongCount: Int? = nil
+    @State private var gameOver = false
+
+    let maxAttempts = 10  // Maximum number of tries per game
+
 }
 
 struct ContentView_Previews: PreviewProvider {
